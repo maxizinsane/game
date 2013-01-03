85729bacbd7c349bcfd2cbdf735412b86a69ee92
Nens <Nens@192.168.0.100>
2013-01-03 17:35:55 -0500

    Derniere modif de Nens

---
+++ Game/src/State/Menu.java
@@ -0,0 +1,52 @@
+package State;
+
+import java.awt.Shape;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.*;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;  
+import org.newdawn.slick.Graphics;  
+import org.newdawn.slick.SlickException;  
+import org.newdawn.slick.state.BasicGameState;  
+import org.newdawn.slick.state.StateBasedGame;  
+
+public class Menu extends BasicGameState {
+
+	public int x = 0;// les coordonnées initiales du message
+    public int y = 0;
+    private Image rectangle;
+    
+	public Menu() {
+		super();
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sgb) throws SlickException {
+		rectangle = new Image("C:\\Users\\Nens\\Desktop\\game-menu3.jpg");
+	}
+	
+	public void render(GameContainer gc, StateBasedGame sgb, Graphics g) throws SlickException {
+		
+		g.setColor(Color.blue);
+		rectangle.draw();g.drawString("mon premier programme en Slick2D", x , 300 );
+		g.drawOval(50, 50, 100, 100);
+	}
+	
+	public void update(GameContainer gc, StateBasedGame sgb, int delta) throws SlickException {
+		
+		if (x<800 && y< 600) {
+			x = x + 1;
+			//y = y +10;
+		}
+		else
+		{
+			x=0;
+			//y=0;
+		}
+	}
+	
+	public int getID(){
+		return 0;
+	}
+}
---
+++ Game/src/State/isometricPlay.java
@@ -0,0 +1,27 @@
+package State;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class isometricPlay extends BasicGameState {
+
+	public isometricPlay(int state) {
+		
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sgb) throws SlickException {
+		
+	}
+	
+	public void render(GameContainer gc, StateBasedGame sgb, Graphics g) throws SlickException {
+		
+	}
+	
+	public void update(GameContainer gc, StateBasedGame sgb, int delta) throws SlickException {
+		
+	}
+	
+	public int getID(){
+		return 1;
+	}
+}
