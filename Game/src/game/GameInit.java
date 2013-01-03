4e27b6be0350cb0d8b23caf47428f02faf1cea89
Nens <Nens@192.168.0.100>
2013-01-03 17:48:32 -0500

    Dernier GameInit de Nens

---
+++ Game/src/game/GameInit.java
@@ -0,0 +1,39 @@
+package game;
+
+/** 
+ **cette classe est le point d'entrée du jeu ! 
+ */  
+import org.newdawn.slick.AppGameContainer;  
+import org.newdawn.slick.GameContainer;  
+import org.newdawn.slick.SlickException;  
+import org.newdawn.slick.state.StateBasedGame;  
+import State.TestJeu;
+
+public class GameInit extends StateBasedGame  {
+
+	private TestJeu testJeu; // le premier état du jeu (voir Menu.java)  
+	private AppGameContainer container; // le conteneur du jeu  
+	public GameInit() {super("Xenobongo");} // le constructeur de la classe  
+	@Override  
+	public void initStatesList(GameContainer container) throws SlickException   
+	{  
+		if (container instanceof AppGameContainer) {  //blabla
+			this.container = (AppGameContainer) container;// on stocke le conteneur du jeu !  
+		}  
+		testJeu = new TestJeu();//le jeu en lui même !!  
+		container.setShowFPS(true);//on ne veut pas voir le FPS ?? mettre alors "false" !  
+		addState(testJeu);    //on ajoute le GameState au conteneur !  
+		
+	}  
+	public static void main(String[] args)   
+	{  
+		try  
+		{  
+			AppGameContainer container = new AppGameContainer(new GameInit());  
+			container.setDisplayMode(1280, 1000, false);// fenêtre de 1280*768 fullscreen =false !!  
+			container.setTargetFrameRate(60);// on règle le FrameRate  
+			container.start();//on démarre le container  
+		}                         
+		catch (SlickException e) {e.printStackTrace();}  // l'exception de base de slick !!  
+	}  
+}  // fin de classe  
